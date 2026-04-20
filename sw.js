5bede7e5f4c4517b901950c1c850d0c437f37cfc
NomX69japan <nomx69japan@gmail.com>
2026-04-20 15:19:43 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+// club 道 TAO POS - Service Worker
+self.addEventListener('push', (event) => {
+  let data = { title: '入店通知', body: '新しいお客様が入店しました' };
+  try { data = event.data.json(); } catch(e) {}
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: data.icon || '/favicon.ico',
+      badge: '/favicon.ico',
+      tag: 'pos-notification',
+      requireInteraction: false,
+      vibrate: [200, 100, 200],
+    })
+  );
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(clients.openWindow('/'));
+});
